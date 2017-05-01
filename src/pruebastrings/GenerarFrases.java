744636f8c1e42331313fc6f595bb5b317aa2bdb8
francisco <francisco@francisco-TravelMate-P256-M>
2017-05-01 20:33:44 +0200

    Cambios mierdas

--- src/pruebastrings/GenerarFrases.java
+++ src/pruebastrings/GenerarFrases.java
@@ -15,6 +15,7 @@ import java.sql.ResultSet;
  * @author Jose
  */
 public class GenerarFrases {
+
     private String frase1;
     private String frase2;
     private String frase3;
@@ -31,74 +32,70 @@ public class GenerarFrases {
     public String getFrase1() {
         return frase1;
     }
+
     public String getFrase2() {
         return frase2;
     }
+
     public String getFrase3() {
         return frase3;
     }
 
-    
     /**
-     * Coge los segmentos de frase de la bbdd y los concatena
-     * Conecta con usuario 'Pruebas_select' y contraseña 'pruebas'
+     * Coge los segmentos de frase de la bbdd y los concatena Conecta con
+     * usuario 'Pruebas_select' y contraseña 'pruebas'
+     *
      * @return String con la frase generada
-     */ 
+     */
     
-    public static String generador(){
-     try {
-
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conexion = DriverManager.getConnection("jdbc:mysql://192.168.1.128/prueba", "Prueba_select", "pruebas");
-            //Statement tiene muchos mÃ©todos, pero hay dos interesentantes: executeUpdate() y executeQuery(). 
-            
-            Statement st = conexion.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM Contacto");
-            while (rs.next()) {
-                //System.out.println("nombre="+rs.getObject("nombre")+
-                //", apellidos="+rs.getObject("apellidos")+
-                //", telefono="+rs.getObject("telefono"));
-                System.out.println("Nombre=" + rs.getObject("nombre")
-                        + ", apellidos=" + rs.getObject("apellidos")
-                        + ", ID=" + rs.getObject(1));
-            }
-            rs.close();            
-
-        } catch (Exception e) {
-            System.out.println("Problema con BD:" + e.getMessage());
-        }
-     return "";
-    } 
+        
      
     
-    public static String generar(){
-        String frase_montada="";    
-        String[] str1,str2,str3;
+
+    public static String generar() {
+        String frase_montada = "";
+        String[] str1, str2, str3;
         /* 
          AQUI VIENE TODO EL GENERAR Y LOS SELECT PARA PILLAR LOS SEGMENTOS DE FRASES
          */
         /**
          * str1= new String["select count(frase1) from frases;"];
          * str1[i]=("Select frase1 from frases where frase1="+i);
-         * 
-         * 
-         * frase1=Select frase1 from frases
-         * frase2=Select frase2 from frases
+         *
+         *
+         * frase1=Select frase1 from frases frase2=Select frase2 from frases
          * frase3=Select frase3 from frases
-         * 
-         */ 
-        
-        /**
-         * for(int i =0;i<str1.length;i++){
-         *     for(int j=0;j<str2.length;j++){
-         *          for(int k=0;k<str3.length;k++){
-         *              frase_montada=str1[i]+" "+str2[j]+" "+str2[k];
-         *          }
-         *     } 
-         * }
+         *
          */
         
-        
-    return frase_montada;
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://192.168.1.128/prueba", "Prueba_select", "pruebas");
+            //Statement tiene muchos mÃ©todos, pero hay dos interesentantes: executeUpdate() y executeQuery(). 
+
+            Statement st = conexion.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM Contacto");
+            while (rs.next()) {
+                //System.out.println("nombre="+rs.getObject("nombre")+
+                //", apellidos="+rs.getObject("apellidos")+
+                //", telefono="+rs.getObject("telefono"));
+                str1[i]=rs.getObject("frase1");
+            }
+            rs.close();
+
+        } catch (Exception e) {
+            System.out.println("Problema con BD:" + e.getMessage());
+        }
+
+        for (int i = 0; i < str1.length; i++) {
+            for (int j = 0; j < str2.length; j++) {
+                for (int k = 0; k < str3.length; k++) {
+                    frase_montada = str1[i] + " " + str2[j] + " " + str2[k];
+                }
+            }
+        }
+
+        return frase_montada;
     }
 }
