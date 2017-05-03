379a5803a50591a814d4fa64287d812d4e909514
Jose <Jose@DESKTOP-6R6K26P>
2017-05-03 14:50:53 +0200

    Añadido la creacion de la base de datos

--- src/pruebastrings/PruebaStrings.java
+++ src/pruebastrings/PruebaStrings.java
@@ -17,7 +17,29 @@ public class PruebaStrings {
     public static void main(String[] args) {
         VentanaInic vi = new VentanaInic();
         vi.setVisible(true);
-        
+        /*
+         * create database prueba;
+
+            create table frases (frase1 varchar(100),frase2 varchar(100),frase3 varchar(100));
+
+
+            create user Pruebas_select@localhost identified by 'pruebas';
+            grant select on prueba.* to Pruebas_select@localhost;
+            flush privileges;
+
+            create user Pruebas_insert@localhost identified by 'pruebas';
+            grant insert on prueba.* to Pruebas_insert@localhost;
+            flush privileges;
+
+            insert into frases (frase1,frase2,frase3)
+            values ("En nuestra tienda podrá encontrar el mejor ","En nuestra tienda podrá encontrar el mejor ", "En nuestra tienda podrá encontrar el mejor ");
+
+            drop table frases;
+
+
+            create user Pruebas_root@localhost identified by 'cagoenla';
+            grant all on prueba.* to Pruebas_root@localhost;
+         */
     }
     
 }
